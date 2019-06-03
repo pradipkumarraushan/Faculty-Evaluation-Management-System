d8fcef836e8c8b1298a5cc1a11802606e41c9a3d
pradipkumarraushan <pradipkumarraushan@gmail.com>
2019-06-04 01:59:04 +0530

    update

--- registration-success-mail.php
+++ registration-success-mail.php
@@ -12,8 +12,8 @@ $mail->Host = "Enter Host name ";
 // set this true if SMTP host requires authentication to send mail
 $mail->SMTPAuth = true;
 //Provide username & password
-$mail->Username = "Enter ypur Email Id";
-$mail->Password = "Enter Your Password";
+$mail->Username = "Enter your Email Id";
+$mail->Password = "Enter your Password";
 $mail->SMTPSecure = "tls";
 $mail->Port = 587;// Enter  port number 
 
@@ -23,16 +23,157 @@ $mail->addAddress($_POST["e"]); // TO email id (Receiver)
 $mail->isHTML(true);
 
 $mail->Subject = "Registration Successful";
-$mail->Body="<div>"."<h2>Faculty Evaluation Management System</h2>"."<p>Hello,</p>".$user[0]["name"]."<br><h3>Your Registration is Successful</h3><br><p>Click here to Login<br>
+$body = "<div>"."<h2>Faculty Evaluation Management System</h2>"."<p>Hello,</p>".$user[0]["name"]."<br><h3>Your Registration is Successful</h3><br><p>Click here to Login<br>
 
-    <a href=' ".PROJECT_NAME."index.php?option=login'>".PROJECT_NAME."index.php?option=login</a><br><br></p>Regards<br> Admin.</div>";
+    <a href=' ".PROJECT_NAME."index.php?option=login'>".PROJECT_NAME."index.php?option=login</a><br><br></p></div>";
+    $body .= '<table id="zs-output-sig" style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse; width: 550px;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828;"><span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828; display: inline;">Kind Regards,</span></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 7px; height: 7px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td width="92">
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="border-collapse: collapse; line-height: 0px;"><img src="https://ucarecdn.com/e4ae6988-225c-4b23-aff5-a1262ad59475/-/resize/92x120/img.png" width="92" height="120" border="0" /></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style="border-collapse: collapse; padding-right: 7px; width: 7px;" width="7">&nbsp;</td>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 25.0px; font-style: normal; line-height: 32px; font-weight: normal; color: #282828;"><span style="font-family: Calibri, Helvetica, sans-serif; font-size: 30.0px; font-style: normal; line-height: 32px; font-weight: normal; color: #282828; display: inline;">PRADIP KUMAR RAUSHAN</span></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 21.0px; font-style: normal; line-height: 23px; font-weight: bold; color: #282828;"><span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 23px; font-weight: bold; color: #282828; display: inline;">CSE - SDM College Of Engineering And Technology.</span></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828;"> <span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828; display: inline;">( Web Developer ).<br><br></span></td>
+</tr> 
+
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828;"><span style="font-family: Calibri, Helvetica, sans-serif; font-size: 14.0px; font-style: normal; line-height: 16px; font-weight: normal; color: #5e4036; display: inline;">Mobile:</span> <span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828; display: inline;">+919035867192</span></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828;"><span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #5e4036; display: inline;">Address:</span> <span style="font-family: Calibri, Helvetica, sans-serif; font-size: 15.0px; font-style: normal; line-height: 17px; font-weight: normal; color: #282828; display: inline;">Karnataka,India.</span></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 3px; height: 3px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style="border-collapse: collapse; padding-right: 8px; width: 8px;">&nbsp;</td>
+<td style="border-collapse: collapse; background-color: #000000; width: 3px;">&nbsp;</td>
+<td style="border-collapse: collapse; padding-right: 8px; width: 8px;">&nbsp;</td>
+<td>
+<table style="font-family: Arial,Helvetica,sans-serif; line-height: 0px; font-size: 1px; padding: 0px; border-spacing: 0px; margin: 0px; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td><a style="font-size: 0px; line-height: 0px;" href="https://www.facebook.com/pradipkumarraushan" rel="nofollow noopener"><img src="https://gimm.io/assets/social/24/171616/04facebook.gif" alt="facebook" width="24" height="24" border="0" /></a></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 5px; height: 5px;">&nbsp;</td>
+</tr>
+<tr>
+<td><a style="font-size: 0px; line-height: 0px;" href="https://github.com/pradipkumarraushan" rel="nofollow noopener"><img src="https://gimm.io/assets/social/24/000000/04github.gif" alt="github" width="24" height="24" border="0" /></a></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 5px; height: 5px;">&nbsp;</td>
+</tr>
+<tr>
+<td><a style="font-size: 0px; line-height: 0px;" href="https://www.linkedin.com/in/pradipkumarraushan/" rel="nofollow noopener"><img src="https://gimm.io/assets/social/24/000000/04linkedin.gif" alt="linkedin" width="24" height="24" border="0" /></a></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 5px; height: 5px;">&nbsp;</td>
+</tr>
+<tr>
+<td><a style="font-size: 0px; line-height: 0px;" title="Email" href="mailto:admin@pradip-github.epizy.com" rel="nofollow"><img src="https://ucarecdn.com/33ed08dc-a3e0-451f-afaf-4091eb842ffb/-/crop/426x280/236,119/-/preview/" alt="https://ucarecdn.com/33ed08dc-a3e0-451f-afaf-4091eb842ffb/-/crop/426x280/236,119/-/preview/" width="26" height="17" border="0" /></a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; padding-bottom: 8px; height: 8px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="border-collapse: collapse;">&nbsp;</td>
+</tr>
+</tbody>
+</table>';
+
+$mail->MsgHTML($body);
 
         if(!$mail->send()) {
             $error_message = "Mailer Error : ". $mail->ErrorInfo;
         } else {
             $success_message = "A Confirmation email has been sent to you. @ ".$user[0]["email"]." ";
 
-             header("Refresh:10; url=index.php");//Refresh:6;
+             //header("Refresh:10; url=index.php");//Refresh:6;
         }
         
 
